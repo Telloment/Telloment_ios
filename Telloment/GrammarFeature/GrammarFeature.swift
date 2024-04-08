dd24a77e2af53b14d8ced46abbecbb3b8b79d05c
박진영 <tear4321@cau.ac.kr>
2024-04-08 17:46:18 +0900

    add gitignore

--- Telloment/GrammarFeature/GrammarFeature.swift
+++ Telloment/GrammarFeature/GrammarFeature.swift
@@ -10,6 +10,7 @@ import Alamofire
 
 class GrammarFeature {
     let apiUrl = "https://jsonplaceholder.typicode.com/todos/1"
+    let PNU_URL = "http://speller.cs.pusan.ac.kr/results"
     
     func getTest(onResponsed:@escaping(Response) -> Void) {
         AF.request(apiUrl,
@@ -22,6 +23,26 @@ class GrammarFeature {
             guard let res = response.value else {return}
             onResponsed(res)
         }
+    }
+    
+    func checkSpell(textToCheck: String) async throws -> String {
+        return try await withCheckedThrowingContinuation { continuation in
+            AF.request(PNU_URL ,
+                       method:.post,
+            parameters: nil,
+                       encoding: URLEncoding.default,
+                       headers: ["Content-Type": "application/x-www-form-urlencoded",
+                                 "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"])
+            .validate(statusCode: 200..<300)
+            .responseString { response in
+                guard let res = response.value else { return }
+                continuation.resume(with: parseResponse(response: response))
+            }
+        }
+    }
+    
+    private func parseResponse(response: String) -> String {
+        return ""
     }
 }
 
